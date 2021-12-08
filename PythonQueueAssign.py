a162af2754775daf9c98a4e652b239380e2e4757
biancaaa74 <91210580+biancaaa74@users.noreply.github.com>
2021-12-07 20:47:21 -0500

    Python Queue

---
+++ PythonQueueAssign.py
@@ -0,0 +1,15 @@
+names = ["Emily", "Bobby", "Josh", "Krystal", "Johnny", "Mark", "Aidan", "Christian", "Misty", "Caleb"]
+for x in range (0,10):
+    names.append(0)
+    names.append(1)
+    names.append(2)
+    names.append(3)
+    names.append(4)
+    names.append(5)
+    names.append(6)
+    names.append(7)
+    names.append(8)
+    names.append(9)
+for x in range (0,10):
+    len(names)
+    print(names.pop(0))
